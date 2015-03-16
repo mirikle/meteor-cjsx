98a6389690595c9e4ab2e9f3cfeac1915aca348b
mirikle <mirikle.wang@gmail.com>
2015-03-16 16:13:52 -0700

    Upgrade coffee-react-transform dependency.
    Signed-off-by: mirikle <mirikle.wang@gmail.com>

--- package.js
+++ package.js
@@ -13,5 +13,7 @@ Package.registerBuildPlugin({
   sources: [
     'compile-cjsx.js'
   ],
-  npmDependencies: {"coffee-react-transform": "2.0.0", "coffee-script": "1.7.1", "source-map": "0.1.32"}
+  npmDependencies: {
+    "coffee-react-transform": "https://github.com/mirikle/coffee-react-transform/tarball/4bde77803147153cd9a43b62092f30e4d4c9ed3c",
+    "coffee-script": "1.7.1", "source-map": "0.1.32"}
 });
